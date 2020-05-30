9dac7389edca5523120c0551556a865771733aca
kashyap <parmar.kashyap@rocketmail.com>
2020-05-30 20:40:05 +0530

    Second commit

--- CURD/asgi.py
+++ CURD/asgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-import os
+import os 
 
 from django.core.asgi import get_asgi_application
 
